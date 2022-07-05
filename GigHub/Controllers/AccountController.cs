75bd427cb7d39cbe91de01100c2e3d4f8a401f52
Gaurav Agrawal <gauravag.1005@gmail.com>
2022-07-06 01:24:14 +0530

    Add Name to Artist - Extending ApplicationUser

--- GigHub/Controllers/AccountController.cs
+++ GigHub/Controllers/AccountController.cs
@@ -151,7 +151,13 @@ namespace GigHub.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser 
+                {
+                    Name= model.Name, 
+                    UserName = model.Email, 
+                    Email = model.Email 
+                };
+
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
