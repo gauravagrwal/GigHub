695eb28aab6a0a61fd3fbb42db9af8b525a9c2c4
Gaurav Agrawal <gauravag.1005@gmail.com>
2022-07-11 12:14:11 +0530

    Implement Cancel a Gig

---
+++ GigHub/Migrations/202207101304320_AddIsCanceledToGigMigration.cs
@@ -0,0 +1,18 @@
+﻿namespace GigHub.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIsCanceledToGigMigration : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Gigs", "IsCanceled", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Gigs", "IsCanceled");
+        }
+    }
+}
