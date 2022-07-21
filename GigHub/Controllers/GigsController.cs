8eb46ff9a8d38b0919a0409be00abe225b614d06
Gaurav Agrawal <gauravag.1005@gmail.com>
2022-07-22 00:29:11 +0530

    Implement Gig Details Page

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -50,6 +50,31 @@ namespace GigHub.Controllers
             return View("Gigs", viewModel);
         }
 
+        public ActionResult Details(int id)
+        {
+            var gig = _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+                .SingleOrDefault(g => g.Id == id);
+
+            if (gig == null)
+                return HttpNotFound();
+
+            var viewModel = new GigDetailsViewModel { Gig = gig };
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var userId = User.Identity.GetUserId();
+
+                viewModel.IsAttending = _context.Attendances
+                    .Any(a => a.GigId == gig.Id && a.AttendeeId == userId);
+
+                viewModel.IsFollowing = _context.Followings
+                    .Any(f => f.FolloweeId == gig.ArtistId && f.FollowerId == userId);
+            }
+            return View("Details", viewModel);
+        }
+
         [HttpPost]
         public ActionResult SearchResult(GigsViewModel viewModel)
         {
