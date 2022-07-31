f91ee8654c2283b7bb41669ca089120d80bfb140
Gaurav Agrawal <gauravag.1005@gmail.com>
2022-07-31 19:44:20 +0530

    Adding Integration tests

--- GigHub/Persistence/Migrations/Configuration.cs
+++ GigHub/Persistence/Migrations/Configuration.cs
@@ -3,7 +3,7 @@
     using GigHub.Persistence;
     using System.Data.Entity.Migrations;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    public sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
